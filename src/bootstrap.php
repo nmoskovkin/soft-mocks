444ee93e068cd37271080a27877ee7028897319a
Mougrim <rinat@mougrim.ru>
2018-08-13 11:01:57 +0300

    [SERVELAB-336]: Prepare for xdebug proxy.
    There are next:
    - soft-mocks init logic was moved to src/init_with_composer.php script from phpunit patch;
    - methods \Badoo\SoftMocks::getRewrittenFilePath() and \Badoo\SoftMocks::getOriginalFilePath() were added.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,27 +1,7 @@
 <?php
-$composerInstall = '';
-foreach ([__DIR__ . '/../../../autoload.php', __DIR__ . '/../vendor/autoload.php'] as $file) {
-    if (file_exists($file)) {
-        $composerInstall = $file;
+namespace Badoo;
 
-        break;
-    }
-}
-$php_parser_dir = dirname($composerInstall) . "/nikic/php-parser/lib/PhpParser/";
-require_once $php_parser_dir . "Autoloader.php";
-\PhpParser\Autoloader::register(true);
-// for prevent autoload problems
-$files = [];
-exec('find ' . escapeshellarg($php_parser_dir) . " -type f -name '*.php'", $files);
-foreach ($files as $file) {
-    require_once $file;
-}
-unset($files, $file);
-
-/* Soft Mocks init */
-require_once(dirname(__DIR__) . "/src/Badoo/SoftMocks.php");
-// @TODO Should be removed after release 2.0
-require_once(dirname(__DIR__) . "/src/QA/SoftMocks.php");
-\Badoo\SoftMocks::init();
-require \Badoo\SoftMocks::rewrite($composerInstall);
-unset($php_parser_dir, $composerInstall);
+$composer_install = require __DIR__ . '/init_with_composer.php';
+/** @noinspection PhpIncludeInspection */
+require $composer_install;
+unset($composer_install);
