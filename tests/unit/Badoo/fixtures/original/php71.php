c9f2ce79e0bdcd2fb04115a83850b8a1e24c501d
Oleg Efimov <o.efimov@corp.badoo.com>
2018-09-18 16:41:01 +0300

    [PLATFORM-9661]: Add more tests

---
+++ tests/unit/Badoo/fixtures/original/php71.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file contains php71 code
+ */
+
+class SomeClass
+{
+    protected const VALUES = ['a', 'b', 'c'];
+
+    public static function getAB()
+    {
+        [
+            $a,
+            ,
+            $b
+        ] = self::VALUES;
+
+        return [$a, $b];
+    }
+}
